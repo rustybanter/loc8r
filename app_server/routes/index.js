ff1a549775a9bc359f301205a536104c3cedb7fd
Bauer <david.bauer@ecolab.com>
2016-07-29 12:33:15 -0500

    First commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var ctrlMain = require('../controllers/main');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', ctrlMain.index);
+
+module.exports = router;
